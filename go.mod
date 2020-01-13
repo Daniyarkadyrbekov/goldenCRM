7988125a8c6b1f1b588e077ee017466c6bce83fd
d.kadyrbekov <d.kadyrbekov@dlg.im>
2020-01-13 22:20:18 +0300

    feat(minimal) for heroku

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/goldenCRM.git
+
+go 1.13
+
+require github.com/gin-gonic/gin v1.5.0
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Printf("hello world!\n")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.GET("/", func(c *gin.Context) {
+		//c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.String(200, "hello world\n")
+	})
+
+	router.Run(":" + port)
 }
